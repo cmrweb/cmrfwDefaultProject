eeef6feaa2dabc4c35b303ecf5746ffee99e11b0
tykrasta <tykrastagames@gmail.com>
2019-06-06 23:56:33 +0200

    add js

--- web/pages/home.php
+++ web/pages/home.php
@@ -1,1 +1,6 @@
-<?= $html->h('1','HOME');?>
+<?= $html->h('1','HOME').
+$html->formOpen('#','post','large dark').
+  $html->input('text','username','Nom','entrer votre nom').
+  $html->textarea('6','msg','Message','msg').
+  $html->button('submit','envoyer','primary'). 
+  $html->formClose();?>
