febfb82a2811c135b64b225a2305a98c83a53527
tykrasta <tykrastagames@gmail.com>
2019-06-07 00:15:23 +0200

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 require 'web/includes/head.php';
 require 'web/includes/header.php';
-require 'web/includes/Route.php';
+require 'web/includes/main.php';
 require 'web/includes/footer.php'; 
 require 'web/includes/script.php'; 
 ?>
