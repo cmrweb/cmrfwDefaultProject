6d7b1b4fedb6eb24f336115e045302dc4562c31d
tykrasta <tykrastagames@gmail.com>
2019-06-08 22:10:16 +0200

    Update cmr.bat

--- lib/cli/cmr.bat
+++ lib/cli/cmr.bat
@@ -1,6 +1,6 @@
 @echo off 
-set /P id=params: 
+set /p cmr="cmr "
 REM Change Directory to the folder containing your script
 CD C:\wamp64\www\cmrfwDefaultProject\lib\cli
 REM Execute
-php cmr.php %id%
+php cmr.php %cmr%
