079ec25bc568a7c79b92663d07358cd97849f9e8
tykrasta <tykrastagames@gmail.com>
2019-06-08 02:58:26 +0200

    Update mod_articles.php

--- web/module/mod_articles.php
+++ web/module/mod_articles.php
@@ -5,6 +5,7 @@ foreach ($article->getData() as $key => $value) : ?>
 	<article class="article" value="<?= $value['post_id']; ?>">
 		<h1><a href="article/<?= $value['post_id']; ?>"><?= $value['title']; ?></a></h1>
 		<p><?= $value['post']; ?></p>
+		<img src="<?=ROOT_DIR.IMG_DIR.'/'.$value["img"]?>" alt="" width="100%">
 	</article>
 <?php	
 endforeach;
