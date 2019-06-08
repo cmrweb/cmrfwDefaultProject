133fbfb53a30c3afcc8482b55660ef2cce2eb139
tykrasta <tykrastagames@gmail.com>
2019-06-08 22:54:34 +0200

    Update db_cmrfw.sql

--- db_cmrfw.sql
+++ db_cmrfw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 15 jan. 2019 à 21:03
+-- Généré le :  sam. 08 juin 2019 à 20:53
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -34,16 +34,9 @@ CREATE TABLE IF NOT EXISTS `cmr_post` (
   `user_id` int(11) NOT NULL,
   `titre` varchar(128) NOT NULL,
   `post` text NOT NULL,
+  `img` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`post_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `cmr_post`
---
-
-INSERT INTO `cmr_post` (`post_id`, `user_id`, `titre`, `post`) VALUES
-(1, 2, 'article 1', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam necessitatibus dolor sint doloribus, quas eaque, aliquam sit veritatis officiis animi assumenda facilis quaerat distinctio magnam quia neque libero eum ipsa!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam necessitatibus dolor sint doloribus, quas eaque, aliquam sit veritatis officiis animi assumenda facilis quaerat distinctio magnam quia neque libero eum ipsa!'),
-(2, 1, 'article 2', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam necessitatibus dolor sint doloribus, quas eaque, aliquam sit veritatis officiis animi assumenda facilis quaerat distinctio magnam quia neque libero eum ipsa!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam necessitatibus dolor sint doloribus, quas eaque, aliquam sit veritatis officiis animi assumenda facilis quaerat distinctio magnam quia neque libero eum ipsa!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam necessitatibus dolor sint doloribus, quas eaque, aliquam sit veritatis officiis animi assumenda facilis quaerat distinctio magnam quia neque libero eum ipsa!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam necessitatibus dolor sint doloribus, quas eaque, aliquam sit veritatis officiis animi assumenda facilis quaerat distinctio magnam quia neque libero eum ipsa!');
+) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -55,17 +48,9 @@ DROP TABLE IF EXISTS `cmr_user`;
 CREATE TABLE IF NOT EXISTS `cmr_user` (
   `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `cmr_user`
---
-
-INSERT INTO `cmr_user` (`user_id`, `username`, `password`) VALUES
-(1, 'tykrasta', '1234'),
-(2, 'cmrweb', '1234');
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
